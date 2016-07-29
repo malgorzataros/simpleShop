4dfc5b39d4b82bcacb1a00e6ab12714f03187bd9
malgorzataros <malgorzataros@wp.pl>
2016-07-29 15:46:02 +0000

    shop

--- src/User.php
+++ src/User.php
@@ -2,26 +2,77 @@
 
 class User {
     
+    public static function SetConnection($newConnection){
+        User::$conn = $newConnection;
+    }
+    
+    public function LogIn($email, $password){
+        $sql = "SELECT * FROM User WHERE User.email = '$email'";
+        $result = User::$conn->query($sql);
+        if($result->num_rows == 1){
+            $row = $result->felch_assoc();
+            if(password_verify($password, $row['password'])){
+                return $row['id'];
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        
+    }
+    
+    static private $conn;
+
     private $id;
     private $name;
     private $surname;
     private $email;
     private $password;
     
-    public function __construct() {
-        $this->id = -1;
-        $this->name = '';
-        $this->surname = '';
-        $this->email = '';
-        $this->password = '';
+    public function __construct($id, $name, $surname, $email, $password) {
+        $this->setId($id);
+        $this->setName($name);
+        $this->setSurname($surname);
+        $this->setEmail($email);
+        $this->setPassword($password);
     }
     
     public function setId($id){
-        $this->id = $id;
+        $this->id = is_numeric($id) ? $id : -1;
     }
     
-    //public function set
+    public function setName($name){
+        $this->name = is_string($name) ? $name:'';
+    }
     
+    public function getName(){
+        return $this->name;
+    }
+    
+    public function setSurname($surname){
+        $this->surname = is_string($surname) ? $surname:'';
+    }
+    
+    public function getSurname(){
+        return $this->surname;
+    }
+    
+    public function setEmail($email){
+        $this->email = is_string($email) ? $email:'';
+    }
+    
+    public function getEmail(){
+        return $this->email;
+    }
+    
+    public function setPassword($password){
+        $this->password = is_string($password) ? password_hash($password, PASSWORD_DEFAULT) : '';
+    }
+    
+    public function getPassword(){
+        return $this->password;
+    }
     
     
     
